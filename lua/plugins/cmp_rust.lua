b892b5c04916c61334421436a3bf446373df40bd
Son Vu <sonvt1710@gmail.com>
2024-02-04 15:49:52 +0700

    first commit

---
+++ lua/plugins/cmp_rust.lua
@@ -0,0 +1,21 @@
+return {
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      {
+        "Saecki/crates.nvim",
+        event = { "BufRead Cargo.toml" },
+        opts = {
+          src = {
+            cmp = { enabled = true },
+          },
+        },
+      },
+    },
+    ---@param opts cmp.ConfigSchema
+    opts = function(_, opts)
+      opts.sources = opts.sources or {}
+      table.insert(opts.sources, { name = "crates" })
+    end,
+  },
+}
---
+++ lua/plugins/lint_terraform.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "mfussenegger/nvim-lint",
+    optional = true,
+    opts = {
+      linters_by_ft = {
+        terraform = { "terraform_validate" },
+        tf = { "terraform_validate" },
+      },
+    },
+  }
+}
